889a7e2f813fd43639efb902a1f8a514661b0fdc
Joao Pedro Costa Lopes <68126578+Joaopclopes@users.noreply.github.com>
2021-11-19 20:05:49 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,6 @@ using namespace std;
 
 int main()
 {
-    ifstream arquivoE;
-    string linha;
-    arquivoE.open("tiktok_app_reviews.csv");
-    if(arquivoE.is_open()){
-        while(getline(arquivoE,linha)){
-            cout<<linha<<endl;
-        }
-    }else{
-        cout<<"Nao foi possivel abrir o arquivo"<<endl;
-    }
-    return 0;
+    
 }
 
