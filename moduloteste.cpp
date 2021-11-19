45e1536e0102cb27322533db37832b2a082e4971
Joao Pedro Costa Lopes <68126578+Joaopclopes@users.noreply.github.com>
2021-11-19 20:01:45 -0300

    Create moduloteste.cpp

---
+++ moduloteste.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "moduloteste.h"
+
+using namespace std;
+
+moduloteste::moduloteste()
+{
+};
+
+moduloteste::~moduloteste()
+{
+};
+
+void moduloteste::acessaRegistro(int i)
+{
+};
+
+void moduloteste::testeImportacao()
+{
+
+}
