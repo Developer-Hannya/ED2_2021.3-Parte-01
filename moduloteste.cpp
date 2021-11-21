b38d28c503d8a52d9f9acd3d4085d10d15d081b1
Wesleyjjx <76188187+Wesleyjjx@users.noreply.github.com>
2021-11-21 14:07:42 -0300

    Update moduloteste.cpp

--- moduloteste.cpp
+++ moduloteste.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "moduloteste.h"
+#include <stdio.h>
 
 using namespace std;
 
@@ -13,6 +14,20 @@ moduloteste::~moduloteste()
 
 void moduloteste::acessaRegistro(int i)
 {
+      FILE *arq;
+    double Vet[100];
+    int result;
+    arq = fopen("ArqTeste.dat", "rb");// Abre um arquivo BINÁRIO para LEITURA
+    if (arq == NULL)  // Se houve erro na abertura
+    {
+        cout<<"Problemas na abertura do arquivo"<<endl;
+        return;
+    }
+    result = fread (&Vet[0], sizeof(double), 100, arq);
+    cout<<"Nro de elementos lidos:"<< result<<endl;
+    cout<<Vet[i]<<endl;
+
+    fclose(arq);
 };
 
 void moduloteste::testeImportacao()
