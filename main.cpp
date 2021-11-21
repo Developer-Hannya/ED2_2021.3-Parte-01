0f2d8be407c4988518f2d2dc39807c30fb4b4282
Joao Pedro Costa Lopes <68126578+Joaopclopes@users.noreply.github.com>
2021-11-20 22:44:08 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,21 +8,16 @@ using namespace std;
 vector<string> importacao(vector<string> registros,int n)
 {
     int i;
-    fstream file;
     vector<string> arranjo;
-
-    file.open(registros,ios::in | ios::out | ios::binary)
-
-    if(file.is_open())
-    {
-        for(i=0,i<n,i++)
-        {
-            int ale;
-            srand (time(NULL));
-            ale = rand() % 3500000;
-            arranjo.push_back(resgistros.at(ale));
-        }
-    }    
+    
+     for(i=0,i<n,i++)
+     {
+        int ale;
+        srand (time(NULL));
+        ale = rand() % 3500000;
+        arranjo.push_back(resgistros.at(ale));
+     }
+    
     return arranjo;
 };
 
