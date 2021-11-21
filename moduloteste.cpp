e87a3cfc5ffe53a0b35dad1aa279250799ae477a
Joao Pedro Costa Lopes <68126578+Joaopclopes@users.noreply.github.com>
2021-11-21 18:52:13 -0300

    Update moduloteste.cpp

--- moduloteste.cpp
+++ moduloteste.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdio.h>
+#include <time.h>
+
 #include "moduloteste.h"
 
 
@@ -15,44 +17,56 @@ moduloteste::~moduloteste()
 
 void moduloteste::acessaRegistro(int i)
 {
-    int i,n;
     double Vet[n];
+   
+    
+    FILE *arq;
+
+    n = 100;
+    int result;
+
+    arq = fopen("ArqTeste.dat", "rb");// Abre um arquivo BINÁRIO para LEITURA
+
+    if (arq == NULL)  // Se houve erro na abertura
+    {
+        cout<<"Problemas na abertura do arquivo"<<endl;
+        return;
+    }
+
+    result = fread (&Vet[0], sizeof(double), 100, arq);
+    cout<<"Nro de elementos lidos:"<< result<<endl;
+    cout<<Vet[i]<<endl;
+
+    fclose(arq);
+    
+
+}
+void moduloteste::testeImportacao()
+{
+    int i;
+
     cout << "[1] exibir saida" << endl << "[2] salvar em arquivo texto" << endl ;
     cin >> i;
+
     if(i == 1)
     {
-        n = 10;
-        
+        FILE *dados;
+        int ale,cout;
+
+        for(cout = 0,cout < 10,cout++)
+        {
+           srand (time(NULL));
+           ale = rand() % 3500000;
+
+        }
     }
     else if(i == 2)
     {
-        FILE *arq;
 
-        n = 100;
-        int result;
-
-        arq = fopen("ArqTeste.dat", "rb");// Abre um arquivo BINÁRIO para LEITURA
-
-        if (arq == NULL)  // Se houve erro na abertura
-        {
-            cout<<"Problemas na abertura do arquivo"<<endl;
-            return;
-        }
-
-        result = fread (&Vet[0], sizeof(double), 100, arq);
-        cout<<"Nro de elementos lidos:"<< result<<endl;
-        cout<<Vet[i]<<endl;
-
-        fclose(arq);
     }
     else
     {
         cout << "ERRO:Operacao invalida."<< endl;
-        return;
-    };
-};
-
-void moduloteste::testeImportacao()
-{
-
+        return; 
+    }
 }
