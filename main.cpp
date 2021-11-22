f0670e2d39c0f8c9c4629e72eba2c2e5052481ae
Developer-Hannya <42482469+Developer-Hannya@users.noreply.github.com>
2021-11-22 16:41:12 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <fstream>
 #include <string>
 #include <time.h>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include "FileReader.h"
+#include "TikTokData.h"
 
 using namespace std;
 
@@ -23,6 +28,16 @@ vector<string> importacao(vector<string> registros,int n)
 
 int main()
 {
-    
+    FileReader r;
+    std::cout << "leitor de arquivo criado" << std::endl;
+    std::vector<TikTokData> cringe;
+    std::cout << "TAD criado" << std::endl;
+    std::cout << "lendo informacoes do arquivo .csv e passando para o TAD" << std::endl;
+    cringe = r.readTikTok("tiktok_app_reviews.csv");
+    std::cout << "informaçoes passadas para o TAD com sucesso" << std::endl;
+    std::cout << "criando o arquivo .bin" << std::endl;
+    std::cout << "passando as informacoes do TAD para o arquivo .bin" << std::endl;
+    r.writeInBinFile(cringe);
+    std::cout << "dados escritos no arquivo .bin" << std::endl;
 }
 
