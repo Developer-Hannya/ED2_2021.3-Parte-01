e10d76141293c89234ff83851c0cb2a0c664a31d
Joao Pedro Costa Lopes <68126578+Joaopclopes@users.noreply.github.com>
2022-01-19 19:38:17 -0300

    Update moduloteste.h

--- moduloteste.h
+++ moduloteste.h
@@ -1,16 +1,15 @@
 #ifndef moduloteste_H_INCLUDED
 #define moduloteste_H_INCLUDED
 
-#include <vector>
 
 class moduloteste
 {
     public:
        moduloteste();
-       ~moduloteste();
+ 
 
-       void acessaRegistro(int i,vector<TikTokData> dados);
-       void testeImportacao(vector<TikTokData> dados);
+       void acessaRegistro(int i);
+       void testeImportacao();
 
 };
 
